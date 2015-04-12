c85256d056660ee06d836c355f874b81f2a17048
athey <hansgudim@gmail.com>
2015-04-12 15:33:38 +0200

    SessionContract created

---
+++ app/src/main/java/org/gudim/android/jogger/SessionContract.java
@@ -0,0 +1,24 @@
+package org.gudim.android.jogger;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by hansg_000 on 12.04.2015.
+ */
+public final class SessionContract {
+    public SessionContract()
+    {
+
+    }
+
+    public static abstract class SessionEntry implements BaseColumns
+    {
+        public static final String TABLE_NAME = "session";
+        public static final String COLUMN_NAME_SESSION_ID = "sessionid";
+        public static final String COLUMN_NAME_TITLE = "sessiontitle";
+        public static final String COLUMN_NAME_DATE = "sessiondate";
+        public static final String COLUMN_NAME_LENGTH = "sessionlength";
+        public static final String COLUMN_NAME_DURATION = "sessionduration";
+        public static final String COLUMN_NAME_IMAGE = "sessionimmage";
+    }
+}
