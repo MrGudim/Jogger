2c23db087fb3a6c5baa6374132edb0e57b48dcb2
athey <hansgudim@gmail.com>
2015-04-23 13:48:34 +0200

    Quick fix

--- app/src/main/java/org/gudim/android/jogger/MyActionBarActivity.java
+++ app/src/main/java/org/gudim/android/jogger/MyActionBarActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class MyActionBarActivity extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
